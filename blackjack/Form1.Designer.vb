01ec5681baccf81e80b15973f9f439315268d62f
eddowii <eddie.hart32@gmail.com>
2018-10-11 09:21:38 +0100

    Add files via upload

---
+++ blackjack/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.cardImages = New System.Windows.Forms.ImageList(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.status = New System.Windows.Forms.ToolStripStatusLabel()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Bank:  £0"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(121, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "&Bet"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.DecimalPlaces = 2
+        Me.NumericUpDown1.Location = New System.Drawing.Point(31, 26)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(84, 20)
+        Me.NumericUpDown1.TabIndex = 3
+        Me.NumericUpDown1.ThousandsSeparator = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "£"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(6, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(281, 47)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "&Hit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox4)
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Enabled = False
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 63)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(756, 400)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Play Area"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.ListView2)
+        Me.GroupBox4.Location = New System.Drawing.Point(6, 19)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(445, 180)
+        Me.GroupBox4.TabIndex = 10
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Dealer"
+        '
+        'ListView2
+        '
+        Me.ListView2.Location = New System.Drawing.Point(6, 19)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(433, 153)
+        Me.ListView2.TabIndex = 6
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.ListView1)
+        Me.GroupBox3.Location = New System.Drawing.Point(6, 205)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(744, 189)
+        Me.GroupBox3.TabIndex = 9
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Your Cards"
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(6, 19)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(732, 164)
+        Me.ListView1.TabIndex = 6
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Enabled = False
+        Me.GroupBox2.Location = New System.Drawing.Point(457, 19)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(293, 180)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Buttons whomst've can be presseth"
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(6, 125)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(281, 47)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "&Double Down"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(6, 72)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(281, 47)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "&Stand"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'cardImages
+        '
+        Me.cardImages.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.cardImages.ImageSize = New System.Drawing.Size(69, 105)
+        Me.cardImages.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.status})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 467)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(780, 22)
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'status
+        '
+        Me.status.BackColor = System.Drawing.SystemColors.Control
+        Me.status.Name = "status"
+        Me.status.Size = New System.Drawing.Size(59, 17)
+        Me.status.Text = "Loading..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(780, 489)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Blackjack"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents cardImages As System.Windows.Forms.ImageList
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents status As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ blackjack/Form1.vb
@@ -0,0 +1,355 @@
+﻿Public Class Form1
+    'define all the things
+    Const minbet As Decimal = 1
+
+    Dim rnd = New Random()
+
+    Dim hasStood As Boolean = False
+
+    Dim bank As Decimal = 10 'initial bank value
+    Dim bet As Decimal
+
+    Dim deck As New Dictionary(Of String, Image)
+    Dim fulldeck As New Dictionary(Of String, Image)
+    Dim mycards As New Dictionary(Of String, Image)
+    Dim dealercards As New Dictionary(Of String, Image)
+    'all the things have been defined
+
+    'resets the deck and adds a standard set of cards to it
+    Sub populate_deck()
+        mycards.Clear()
+        dealercards.Clear()
+        deck.Clear()
+        cardImages.Images.Clear()
+        deck.Add("AH", My.Resources.AH)
+        deck.Add("2H", My.Resources._2H)
+        deck.Add("3H", My.Resources._3H)
+        deck.Add("4H", My.Resources._4H)
+        deck.Add("5H", My.Resources._5H)
+        deck.Add("6H", My.Resources._6H)
+        deck.Add("7H", My.Resources._7H)
+        deck.Add("8H", My.Resources._8H)
+        deck.Add("9H", My.Resources._9H)
+        deck.Add("10H", My.Resources._10H)
+        deck.Add("JH", My.Resources.JH)
+        deck.Add("QH", My.Resources.QH)
+        deck.Add("KH", My.Resources.KH)
+        deck.Add("AC", My.Resources.AC)
+        deck.Add("2C", My.Resources._2C)
+        deck.Add("3C", My.Resources._3C)
+        deck.Add("4C", My.Resources._4C)
+        deck.Add("5C", My.Resources._5C)
+        deck.Add("6C", My.Resources._6C)
+        deck.Add("7C", My.Resources._7C)
+        deck.Add("8C", My.Resources._8C)
+        deck.Add("9C", My.Resources._9C)
+        deck.Add("10C", My.Resources._10C)
+        deck.Add("JC", My.Resources.JC)
+        deck.Add("QC", My.Resources.QC)
+        deck.Add("KC", My.Resources.KC)
+        deck.Add("AD", My.Resources.AD)
+        deck.Add("2D", My.Resources._2D)
+        deck.Add("3D", My.Resources._3D)
+        deck.Add("4D", My.Resources._4D)
+        deck.Add("5D", My.Resources._5D)
+        deck.Add("6D", My.Resources._6D)
+        deck.Add("7D", My.Resources._7D)
+        deck.Add("8D", My.Resources._8D)
+        deck.Add("9D", My.Resources._9D)
+        deck.Add("10D", My.Resources._10D)
+        deck.Add("JD", My.Resources.JD)
+        deck.Add("QD", My.Resources.QD)
+        deck.Add("KD", My.Resources.KD)
+        deck.Add("AS", My.Resources._AS)
+        deck.Add("2S", My.Resources._2S)
+        deck.Add("3S", My.Resources._3S)
+        deck.Add("4S", My.Resources._4S)
+        deck.Add("5S", My.Resources._5S)
+        deck.Add("6S", My.Resources._6S)
+        deck.Add("7S", My.Resources._7S)
+        deck.Add("8S", My.Resources._8S)
+        deck.Add("9S", My.Resources._9S)
+        deck.Add("10S", My.Resources._10S)
+        deck.Add("JS", My.Resources.JS)
+        deck.Add("QS", My.Resources.QS)
+        deck.Add("KS", My.Resources.KS)
+        For Each pair As KeyValuePair(Of String, Image) In deck
+            cardImages.Images.Add(pair.Key, pair.Value)
+        Next
+        cardImages.Images.Add("back", My.Resources.yellow_back)
+        ListView1.LargeImageList = cardImages
+        ListView1.SmallImageList = cardImages
+        ListView2.LargeImageList = cardImages
+        ListView2.SmallImageList = cardImages
+        fulldeck = deck
+    End Sub
+    'selects a random card returns it and removes it from the deck
+    Function draw_card() As KeyValuePair(Of String, Image)
+        Try
+            Dim key = deck.Keys.ToList(rnd.Next(0, deck.Keys.ToList.Count))
+            For Each pair As KeyValuePair(Of String, Image) In deck
+                If pair.Key = key Then
+                    deck.Remove(key)
+                    Return pair
+                End If
+            Next
+        Catch ex As Exception
+            MsgBox("i seem to have ran out of cards lmao rip")
+            End
+        End Try
+    End Function
+    'picks a random card from the deck but does not remove it
+    'card peek
+    'unused but why not leave it in
+    Function rand_card() As KeyValuePair(Of String, Image)
+        Dim key = deck.Keys.ToList(rnd.Next(0, deck.Keys.ToList.Count))
+        For Each pair As KeyValuePair(Of String, Image) In deck
+            If pair.Key = key Then
+                Return pair
+            End If
+        Next
+    End Function
+    'updates GUI controls with bank value
+    Sub update_bank()
+        'round bank
+        bank = Decimal.Round(bank, 2, MidpointRounding.AwayFromZero)
+        'update the stuff
+        NumericUpDown1.Maximum = bank
+        Label1.Text = "Bank: £" + bank.ToString("0.00")
+    End Sub
+    'this function is largely irrelevant but I added it in because sophie was complaining
+    'when i suggested adding it
+    Function get_english_card_name(ByVal name As String)
+        Dim suit = name.Substring(name.Length - 1)
+        Dim val = name.Substring(0, name.Length - 1)
+        Select Case suit
+            Case "D"
+                suit = "Diamonds"
+            Case "H"
+                suit = "Hearts"
+            Case "C"
+                suit = "Clubs"
+            Case "S"
+                suit = "Spades"
+        End Select
+        Select Case val
+            Case "A"
+                val = "Ace"
+            Case "J"
+                val = "Jack"
+            Case "Q"
+                val = "Queen"
+            Case "K"
+                val = "King"
+        End Select
+        Return val + " of " + suit
+    End Function
+    'gives the user a card
+    Sub give_user_card()
+        Dim card As KeyValuePair(Of String, Image)
+        card = draw_card()
+        mycards.Add(card.Key, card.Value)
+        ListView1.Items.Add(get_english_card_name(card.Key), card.Key)
+    End Sub
+    'gives the dealer a card
+    Sub give_dealer_card()
+        Dim card As KeyValuePair(Of String, Image)
+        card = draw_card()
+        dealercards.Add(card.Key, card.Value)
+        If Not hasStood Then
+            If dealercards.Count = 1 Then
+                ListView2.Items.Add(get_english_card_name(card.Key), card.Key)
+            Else
+                ListView2.Items.Add("???", "back")
+            End If
+        Else
+            ListView2.Items.Add(get_english_card_name(card.Key), card.Key)
+        End If
+    End Sub
+    'place bet
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If NumericUpDown1.Value >= minbet Then
+            bet = NumericUpDown1.Value
+            NumericUpDown1.Enabled = False
+            Button1.Enabled = False
+            GroupBox1.Enabled = True
+            give_user_card()
+            give_user_card()
+            give_dealer_card()
+            give_dealer_card()
+            'check blackjacks
+            If cards_total(mycards) = 21 Then
+                If cards_total(dealercards) = 21 Then
+                    'tie, give money back
+                    win(0)
+                Else
+                    'you win lmao
+                    win(1)
+                End If
+            Else
+                If cards_total(dealercards) = 21 Then
+                    'dealer wins lol rip
+                    bust()
+                End If
+            End If
+            GroupBox2.Enabled = True
+            status.Text = "Waiting for user action"
+        Else
+            MessageBox.Show("Invalid bet. You must bet at least £" + minbet.ToString("0.00"), "Invalid bet", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        populate_deck()
+        update_bank()
+        'enable all the buttons
+        NumericUpDown1.Minimum = minbet
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        ListView1.Items.Clear()
+        ListView2.Items.Clear()
+        hasStood = False
+        status.Text = "Waiting for bet to be placed"
+    End Sub
+
+    Function cards_total(ByVal cards As Dictionary(Of String, Image))
+        Dim total As Integer = 0
+        Dim aces As Integer = 0
+        For Each pair As KeyValuePair(Of String, Image) In cards
+            Dim val As String = pair.Key.Substring(0, pair.Key.Length - 1)
+            If IsNumeric(val) Then
+                total += Convert.ToInt32(val)
+            ElseIf val = "K" Or val = "Q" Or val = "J" Then
+                total += 10
+            ElseIf val = "A" Then
+                total += 11
+                aces += 1
+            End If
+        Next
+        If total > 21 And aces > 0 Then
+            For i As Integer = 1 To aces
+                total -= 10
+                If total <= 21 Then
+                    Return total
+                End If
+            Next
+        End If
+        Return total
+    End Function
+
+    Sub bust(Optional ByVal total As String = "")
+        bank -= bet
+        update_bank()
+        If total = "" Then
+            MessageBox.Show("The dealer beat you ", "Lmao rip", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        Else
+            MessageBox.Show("Your total was " + total + ", and you went bust! Thanks for the money bro x", "Lmao rip", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        End If
+        If bank < minbet Then
+            MessageBox.Show("You have no money left. Get out of my casino", "Get out of my casino", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+            Me.Close()
+        End If
+        NumericUpDown1.Enabled = True
+        Button1.Enabled = True
+        GroupBox1.Enabled = False
+        GroupBox2.Enabled = False
+        'reset everything
+        Form1_Load(New System.Object, New System.EventArgs)
+    End Sub
+
+    Sub win(ByVal multip As Decimal)
+        bank += bet * multip
+        update_bank()
+        MessageBox.Show("You just won £" + (bet * multip).ToString("0.00") + ". Well done", "Ayyy nice one bro", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        NumericUpDown1.Enabled = True
+        Button1.Enabled = True
+        GroupBox1.Enabled = False
+        GroupBox2.Enabled = False
+        'reset everything
+        Form1_Load(New System.Object, New System.EventArgs)
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        'hit
+        give_user_card()
+        If cards_total(mycards) > 21 Then
+            'bust
+            bust(cards_total(mycards))
+        Else
+            'disable double down
+            Button4.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Button4.Enabled = False
+        'stand
+        If Button4.Enabled Then 'button 4 is disabled after the first action, so this basically means "is it the first action"
+            If cards_total(mycards) = 21 Then
+                'win back original + *1.5
+                win(1.5)
+            Else
+                'regular stand
+                stand()
+            End If
+        Else 'regular stand
+            stand()
+        End If
+    End Sub
+
+    Sub stand()
+        If Not hasStood Then
+            hasStood = True
+            'disable all the buttons
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            'show dealers cards
+            ListView2.Items.Clear()
+            For Each card As KeyValuePair(Of String, Image) In dealercards
+                ListView2.Items.Add(get_english_card_name(card.Key), card.Key)
+            Next
+        End If
+        If cards_total(dealercards) < 17 Then
+            give_dealer_card()
+            stand()
+        Else
+            If cards_total(dealercards) > 21 Then
+                win(1)
+            Else
+                If cards_total(mycards) > cards_total(dealercards) Then
+                    win(1)
+                ElseIf cards_total(mycards) < cards_total(dealercards) Then
+                    bust()
+                Else
+                    win(0)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        'double down
+        If bet * 2 > bank Then
+            MessageBox.Show("You can't do that. You don't have enough money rip", "No", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        Else
+            bet = bet * 2
+            NumericUpDown1.Value = bet
+            give_user_card()
+            If cards_total(mycards) > 21 Then
+                'bust
+                bust(cards_total(mycards))
+            Else
+                'disable double down
+                Button4.Enabled = False
+                stand()
+            End If
+        End If
+    End Sub
+
+    Private Sub ListView2_Click(sender As System.Object, e As System.EventArgs) Handles ListView2.Click
+        MessageBox.Show("You assaulted the dealer. Get out of my casino", "DONT TOUCH THE DEALERS CARDS", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+        Me.Close()
+    End Sub
+End Class
